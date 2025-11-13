c9726905e40dabab0421b5f34e90eb421d193b4b
ASL <s233954@dtu.dk>
2025-11-13 16:13:15 +0100

    userHandling server logic added

--- index.js
+++ index.js
@@ -2,32 +2,89 @@ import e from "express";
 import cors from 'cors';
 import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv';
-dotenv.config({path: './supabase.env'});
+dotenv.config({ path: './supabase.env' });
 //const dotenv = dotenv.config();
 const supabaseUrl = 'https://kjtbrqhsvdakypduvhdp.supabase.co'
-const supabaseKey =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqdGJycWhzdmRha3lwZHV2aGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNTA0NjUsImV4cCI6MjA3NzgyNjQ2NX0.dQ7A4waTq4d1TqXMZs9ppAM3KvR0kd2AzMKCh7yYqSs'//'process.env.supabaseKey'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqdGJycWhzdmRha3lwZHV2aGRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNTA0NjUsImV4cCI6MjA3NzgyNjQ2NX0.dQ7A4waTq4d1TqXMZs9ppAM3KvR0kd2AzMKCh7yYqSs'//'process.env.supabaseKey'
 const supabase = createClient(supabaseUrl, supabaseKey)
 const app = e();
 const port = 3000;
 
 app.use(cors()) //To parse data from different origins (urls)
 app.use(e.json()); //To parse json objects on requests
+25
 
+app.post('/signUp', async (req, res) => {
+    const { email, password, name, age, studie } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        options: {
+            data: {
+                name: name,
+                age: age,
+                studie: studie
+            }
+        }
+    })
+    if (error) {
+        console.error("supabase signUp error:", error.message);
+        return res
+            .status(400)
+            .json({ message: error.message });
+    }
+
+    return res.status(201).json({
+        message: "User signed up successfully",
+        user: data.user
+    });
+})
+
+app.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email or password is wrong" });
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password
+    })
+
+
+    if (error) {
+        console.error("subabase login error:", error.message);
+        return res.status(400).json({ message: error.message });
+    }
+
+    return res.json({
+        message: "User logged in successfully",
+        user: data.user,
+        session: data.session
+    });
+})
 
 /**
  * Post new costume to database
  */
-app.post('/addItemToDatabase', async (req,res) => {
+app.post('/addItemToDatabase', async (req, res) => {
 
-    const {userId} = req.body;
+    const { userId } = req.body;
 
     let { data: users, error } = await supabase
-    .from('users')
-    .select()
-    .eq('id', '1')
+        .from('users')
+        .select()
+        .eq('id', '1')
     console.log(users)
 
-    const {name,description,size} = req.body
+    const { name, description, size } = req.body
     console.log(req.body);
     //const userId = req.body;
 
@@ -38,36 +95,36 @@ app.post('/addItemToDatabase', async (req,res) => {
  */
 app.get('/fetchNewlyItems', async (req, res) => {
     let { data: items, error } = await supabase
-    .from('costumes')
-    .select()
+        .from('costumes')
+        .select()
 
-    if(error){
+    if (error) {
         console.log(error);
     }
 
-    res.json({success: true, costumes: items});
+    res.json({ success: true, costumes: items });
 })
 
 /**
  * To fetch own user items based on session id (mangler sessionId fra login)
  */
-app.get('/fetchUserItems', async(req, res) => {
+app.get('/fetchUserItems', async (req, res) => {
     const userId = 1 //Todo implement as sessionId
-    
+
     let { data: items, error } = await supabase
-    .from('inventory')
-    .select(`
+        .from('inventory')
+        .select(`
         id,
         owner,
         items(id,item_name,item_size,item_description)
     `)
-    .eq('owner',userId);
+        .eq('owner', userId);
 
-    if(error){
+    if (error) {
         console.log(error)
     }
 
-    res.json({success: true, costumes: items})
+    res.json({ success: true, costumes: items })
 
 
     //res.json({success: true, costumes: items});
@@ -78,17 +135,17 @@ app.get('/fetchUserItems', async(req, res) => {
  */
 app.get('/fetchUserItems/:inventoryId', async (req, res) => {
     const inventoryId = req.params.inventoryId;
-    
-    let { data: items, error } = await supabase
-    .from('items')
-    .select()
-    .eq('inventory_id', inventoryId)
 
-    if(error){
+    let { data: items, error } = await supabase
+        .from('items')
+        .select()
+        .eq('inventory_id', inventoryId)
+
+    if (error) {
         console.log(error)
     }
 
-    res.json({success: true, items: items});
+    res.json({ success: true, items: items });
 })
 
 
