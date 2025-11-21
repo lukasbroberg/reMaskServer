14bc3b1563d7adbf05ad765080d6a2dfb92021cb
LRBDeveloper <146123222+LRBDeveloper@users.noreply.github.com>
2025-11-21 20:52:17 +0100

    merged

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -36,28 +36,49 @@ router.post('/signUp', async (req, res) => {
 
 router.post('/login', async (req, res) => {
 
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    if (!email || !password) {
-        return res.status(400).json({ message: "Email or password is wrong" });
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email or password is wrong" });
+        }
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        })
+
+        if (error) {
+            console.error("subabase login error:", error.message);
+            return res.status(400).json({ message: error.message });
+        }
+        const { user, session } = data;
+
+        if (!session || !session.access_token) {
+            console.error("No session returned from supabase");
+            return res
+                .status(500)
+                .json({ message: "No session returned from auth" });
+        }
+
+        return res
+            .cookie("sb_access_token", session.access_token, {
+                httpOnly: true,
+                secure: false, // Set to true if using HTTPS
+                sameSite: 'lax',
+                maxAge: 1000 * 60 * 60 // 1 time
+            })
+            .json({ message: "User logged in successfully" });
+    } catch (err) {
+        console.error("Unexpected error during login:", err);
+        return res.status(500).json({ message: "Internal server error" });
     }
 
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password
-    })
-
-
-    if (error) {
-        console.error("subabase login error:", error.message);
-        return res.status(400).json({ message: error.message });
-    }
-
-    return res.json({
+    /*return res.json({
         message: "User logged in successfully",
         user: data.user,
         session: data.session
-    });
+    });*/
 })
 
 
